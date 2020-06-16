156077dfe831921845ba8bedfc73d2956a0cd26b
Frehzy <55092493+Frehzy@users.noreply.github.com>
2020-06-16 15:21:36 +0500

    Add files via upload

---
+++ GenPass/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace GenPass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroCheckBox2 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroCheckBox3 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroCheckBox4 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTrackBar1 = new MetroFramework.Controls.MetroTrackBar();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(23, 72);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(79, 17);
+            this.metroCheckBox1.TabIndex = 0;
+            this.metroCheckBox1.Text = "Символы";
+            this.metroCheckBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // metroCheckBox2
+            // 
+            this.metroCheckBox2.AutoSize = true;
+            this.metroCheckBox2.Location = new System.Drawing.Point(23, 113);
+            this.metroCheckBox2.Name = "metroCheckBox2";
+            this.metroCheckBox2.Size = new System.Drawing.Size(67, 17);
+            this.metroCheckBox2.TabIndex = 1;
+            this.metroCheckBox2.Text = "Цифры";
+            this.metroCheckBox2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroCheckBox2.UseVisualStyleBackColor = true;
+            // 
+            // metroCheckBox3
+            // 
+            this.metroCheckBox3.AutoSize = true;
+            this.metroCheckBox3.Location = new System.Drawing.Point(23, 158);
+            this.metroCheckBox3.Name = "metroCheckBox3";
+            this.metroCheckBox3.Size = new System.Drawing.Size(127, 17);
+            this.metroCheckBox3.TabIndex = 2;
+            this.metroCheckBox3.Text = "Заглавные буквы";
+            this.metroCheckBox3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroCheckBox3.UseVisualStyleBackColor = true;
+            // 
+            // metroCheckBox4
+            // 
+            this.metroCheckBox4.AutoSize = true;
+            this.metroCheckBox4.Checked = true;
+            this.metroCheckBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.metroCheckBox4.Location = new System.Drawing.Point(23, 201);
+            this.metroCheckBox4.Name = "metroCheckBox4";
+            this.metroCheckBox4.Size = new System.Drawing.Size(123, 17);
+            this.metroCheckBox4.TabIndex = 3;
+            this.metroCheckBox4.Text = "Строчные буквы";
+            this.metroCheckBox4.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroCheckBox4.UseVisualStyleBackColor = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 290);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(395, 45);
+            this.metroButton1.TabIndex = 4;
+            this.metroButton1.Text = "Сгенерировать пароль";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTrackBar1
+            // 
+            this.metroTrackBar1.BackColor = System.Drawing.Color.Transparent;
+            this.metroTrackBar1.Location = new System.Drawing.Point(23, 261);
+            this.metroTrackBar1.Maximum = 30;
+            this.metroTrackBar1.Name = "metroTrackBar1";
+            this.metroTrackBar1.Size = new System.Drawing.Size(395, 23);
+            this.metroTrackBar1.TabIndex = 5;
+            this.metroTrackBar1.Text = "metroTrackBar1";
+            this.metroTrackBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTrackBar1.Value = 15;
+            this.metroTrackBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.MetroTrackBar1_Scroll);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 238);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(103, 20);
+            this.metroLabel1.TabIndex = 6;
+            this.metroLabel1.Text = "Длина пароля:";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(158, 238);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(22, 20);
+            this.metroLabel2.TabIndex = 7;
+            this.metroLabel2.Text = "15";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(23, 397);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(184, 45);
+            this.metroButton2.TabIndex = 9;
+            this.metroButton2.Text = "Скопировать пароль";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Location = new System.Drawing.Point(23, 353);
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.Size = new System.Drawing.Size(395, 23);
+            this.metroTextBox1.TabIndex = 10;
+            this.metroTextBox1.Text = "Пароль";
+            this.metroTextBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(234, 397);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(184, 45);
+            this.metroButton3.TabIndex = 11;
+            this.metroButton3.Text = "Выйти";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 463);
+            this.ControlBox = false;
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTrackBar1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroCheckBox4);
+            this.Controls.Add(this.metroCheckBox3);
+            this.Controls.Add(this.metroCheckBox2);
+            this.Controls.Add(this.metroCheckBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(441, 463);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(441, 463);
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Text = "Генератор паролей";
+            this.TextAlign = System.Windows.Forms.VisualStyles.HorizontalAlign.Center;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox2;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox3;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox4;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTrackBar metroTrackBar1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton3;
+    }
+}
+
---
+++ GenPass/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace GenPass
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            metroLabel2.Text = "15";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            metroTrackBar1.Minimum = 1;
+            metroTrackBar1.Maximum = 30;
+            WindowState = FormWindowState.Minimized;
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                metroTextBox1.Text = "";
+                string abc = "";
+                if (metroCheckBox1.Checked)//использовать спецсимволы
+                    abc += "!@#$%^&*()";
+                if (metroCheckBox2.Checked)//использовать цифры
+                    abc += "123456789";
+                if (metroCheckBox3.Checked)//использовать заглавные
+                    abc += "QWERTYUIOPASDFGHJKLZXCVBNM";
+                if (metroCheckBox4.Checked)//использовать строчные
+                    abc += "qwertyuiopasdfghjklzxcvbnm";
+                Random rnd = new Random();
+                for (int i = 0; i < metroTrackBar1.Value; i++)
+                    metroTextBox1.Text += abc[rnd.Next(abc.Length)];
+            }
+            catch
+            {
+                MessageBox.Show("Похоже, вы не выбрали никакой чек-бокс", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MetroTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            metroLabel2.Text = metroTrackBar1.Value.ToString();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(metroTextBox1.Text);
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+            Close();
+            Environment.Exit(0);
+            Application.Exit();
+        }
+    }
+}
